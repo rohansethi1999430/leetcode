a9380d2ebbf4e00a83d55a6f0dc65fc8e66fec20
rohansethi <rohansethi1999430@gmail.com>
2024-07-07 19:34:09 -0400

    Two Sum Problem

---
+++ TwoSum problem.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+       int[] myArray = new int[5];
+        for(int i =0;i<nums.length,i++){
+            for(int j = i+1;j<nums.length,j++){
+                if(nums[i]+nums[j]==target){
+                    myArray[0] = i;
+                    myArray[1] = j;
+
+                }
+            }
+        }
+        return myArray;
+    }
+}
