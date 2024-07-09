c5564dc3edd9c8274acdd1bc1bfa363ab521f3f9
Rohan Sethi <48809939+rohansethi1999430@users.noreply.github.com>
2024-07-09 12:46:29 -0400

    Update Contains Duplicate.java

--- Contains Duplicate.java
+++ Contains Duplicate.java
@@ -3,17 +3,10 @@ class Solution {
        Map<Integer, Integer> mp = new HashMap<>();
         for (int i =0;i<nums.length;i++){
             if(mp.containsKey(nums[i])){
-                mp.put(nums[i],mp.get(nums[i])+1);
+                return true;
             }
             else{
                 mp.put(nums[i],1);
-            }
-
-        }
-        for (Map.Entry<Integer, Integer> entry : mp.entrySet())
-        {
-            if(entry.getValue()>1){
-                return true;
             }
         }
         return false;
